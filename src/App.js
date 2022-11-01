c8ae2baf63b1f4343d07b672adab5f10a13fd27a
Yuni Song <yuso@itu.dk>
2022-11-01 17:32:07 +0100

    Create login, sign up and homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import Login from './loginpage';
+import Register from './registerpage';
 import './App.css';
+
+
+import Button from 'react-bootstrap/Button';
+
+
 
 function App() {
   return (
-    <div className="App">
+    <main  className="App">
+      <Login />
       <header className="App-header">
-        <h1>Hello StudentIT whats up</h1>
+      <h1>Hello StudentIT whats up</h1>
+      <p></p>
+      <p></p>
+      <p></p>
+      
+      <Button variant="link" size="lg">Sign In</Button>{' '}
+      
+
       </header>
-    </div>
+    </main>
   );
 }
 
