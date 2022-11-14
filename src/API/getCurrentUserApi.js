6e8d929ab41483d2837bbe5e5cf196e7e8f83d49
wimrijkss <80752989+wimrijkss@users.noreply.github.com>
2022-11-14 10:28:22 +0100

    API tries

--- src/API/getCurrentUserApi.js
+++ src/API/getCurrentUserApi.js
@@ -1,16 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import Parse from 'parse/dist/parse.min.js';
 import { LoginContext } from '../../contexts/LoginContext'
 
-export const getCurrentUserApi = () => {
+export const GetCurrentUserApi = () => {
 
-    // const { currentUser, setCurrentUser }  = useContext(LoginContext);
+    const { currentUser, setCurrentUser }  = useContext(LoginContext);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [ errorMessage, setErrorMessages ] = useState("");
 
-    const getCurrentUser = async function () {
+    async function getCurrentUser () {
         const currentUser = await Parse.User.current();
         // Update state variable holding current user
         setCurrentUser(currentUser);
         return currentUser;
+    };
+
+    const doUserLogIn = async function () {
+        // Note that these values come from state variables that we've declared before
+        const usernameValue = username;
+        const passwordValue = password;
+        try {
+        const loggedInUser = await Parse.User.logIn(usernameValue, passwordValue);
+        // To verify that this is in fact the current user, `current` can be used
+        const currentUser = await Parse.User.current();
+        console.log(loggedInUser === currentUser);
+        // Clear input fields
+        setUsername('');
+        setPassword('');
+        // Update state variable holding current user
+        getCurrentUser();
+        return true;
+        } catch (error) {
+        // Error can be caused by wrong parameters or lack of Internet connection
+            // alert(`Error! ${error.message}`);
+            setErrorMessages(error.message)
+            return false;
+        }
     };
 
 }
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import Parse from 'parse/dist/parse.min.js';
 import { LoginContext } from '../../contexts/LoginContext'
 
-import { getCurrentUserApi } from '../../API/getCurrentUserApi';
+import { GetCurrentUserApi } from '../../API/GetCurrentUserApi';
 
 // import '../../common.css';
 import './login.css';
@@ -11,13 +11,13 @@ import { Link } from "react-router-dom";
 
 export function Login() {
     // State variables
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [ errorMessage, setErrorMessages ] = useState("");
+    // const [username, setUsername] = useState("");
+    // const [password, setPassword] = useState("");
+    // const [ errorMessage, setErrorMessages ] = useState("");
 
-    const { currentUser, setCurrentUser }  = useContext(LoginContext);
+    // const { currentUser, setCurrentUser }  = useContext(LoginContext);
 
-    // // Function that will return current user and also update current username
+    // Function that will return current user and also update current username
     // const getCurrentUser = async function () {
     //     const currentUser = await Parse.User.current();
     //     // Update state variable holding current user
@@ -25,30 +25,7 @@ export function Login() {
     //     return currentUser;
     // };
 
-    <getCurrentUserApi />
-
-    const doUserLogIn = async function () {
-        // Note that these values come from state variables that we've declared before
-        const usernameValue = username;
-        const passwordValue = password;
-        try {
-        const loggedInUser = await Parse.User.logIn(usernameValue, passwordValue);
-        // To verify that this is in fact the current user, `current` can be used
-        const currentUser = await Parse.User.current();
-        console.log(loggedInUser === currentUser);
-        // Clear input fields
-        setUsername('');
-        setPassword('');
-        // Update state variable holding current user
-        getCurrentUser();
-        return true;
-        } catch (error) {
-        // Error can be caused by wrong parameters or lack of Internet connection
-            // alert(`Error! ${error.message}`);
-            setErrorMessages(error.message)
-            return false;
-        }
-    };
+    <GetCurrentUserApi />
 
     const renderErrorMessage = () =>(
         <div className="error">{errorMessage}</div>
