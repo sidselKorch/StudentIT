9eeb5512506ec7816fdb57c465906f73d1989140
wimrijkss <80752989+wimrijkss@users.noreply.github.com>
2022-11-28 09:37:04 +0100

    Sidebar elements created
    Populating the sidebar with users from DB

---
+++ src/components/sidebarUserComponent/SidebarUserComponent.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect} from 'react';
+import Parse from 'parse/dist/parse.min.js';
+
+import "./sidebarUserComponent.css"
+
+function SidebarUserComponent(props) {
+    const [ userData, setUserdata ] = useState([])
+    const [ clickedId, setClickedId ] = useState("")
+
+    const [ searchTerm, setSearchTerm ] = useState("");
+
+    function fetchUserData () {
+        var query = new Parse.Query(props.first);
+        query.ascending(props.second);
+        query.find().then((results) => {
+            setUserdata(results)
+        })
+    }
+
+    async function getText(name){
+        const PublisherAQuery = new Parse.Query('Nickname');
+        PublisherAQuery.equalTo('name', name);
+        const PublisherA = await PublisherAQuery.first();
+
+        // Query Books with PublisherA
+        const bookQuery = new Parse.Query('Message');
+        bookQuery.equalTo('sender', PublisherA);
+        let queryResults = await bookQuery.find();
+
+        // Let's show the results
+        for (let result of queryResults) {
+            // You access `Parse.Objects` attributes by using `.get`
+            console.log(result.get('text'));
+        };
+    }
+
+    useEffect(() => {
+        fetchUserData()
+    },[])
+    
+    console.log(userData)
+    function resetClickedId(){
+        setClickedId("")
+    }
+
+    return (
+        <div className="sidebar-user-component">
+            <h2>{props.title}</h2>
+            <p>Clicked Element's ID: {clickedId}</p>
+            <button className="btn" onClick={() => resetClickedId()}>Close Chat</button>
+            <div className="input-field">
+                <input type="text" placeholder="Search..." onChange={(event) => {setSearchTerm(event.target.value)}}></input>
+            </div>
+
+            <div className="sidebar-user-container">
+            {userData.filter((val) => {
+                if (searchTerm == "") {
+                    return val
+                } else if (val.get(props.second).toLowerCase().includes(searchTerm.toLocaleLowerCase())){
+                    return val
+                }
+            }).map((val) => {
+                return <div className={`sidebar-user ${clickedId === val.id ? "clicked" : ""}`}  key={val.id} onClick={() => setClickedId(val.id)}>
+                <div className="user-icon">
+                    <p className="name-initials">{String(val.get(props.second)).substring(0, 1)}{String(val.get(props.third)).substring(0, 1)}</p>
+                </div>
+                <div className="sidebar-user-info">
+                    <p>{val.get(props.second)} {val.get(props.third)}</p>
+                    <p>Here goes a text message that was sent by the sender</p>
+                </div>
+                {}
+            </div>
+            })
+            }
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default SidebarUserComponent
