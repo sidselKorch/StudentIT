6f1f7c860581b799bec2d00eaf02a71fbb54c3c2
wimrijkss <80752989+wimrijkss@users.noreply.github.com>
2022-12-02 19:52:37 +0100

    Sign up courses functionality

--- src/pages/signUpPage/SignUpPage.js
+++ src/pages/signUpPage/SignUpPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Parse from 'parse/dist/parse.min.js';
 
@@ -20,6 +20,59 @@ function SignUp() {
 
   const { getCurrentUser } = useCurrentUserHook()
 
+  
+
+  const courseObj = [
+    {"Courses":"Advanced Algorithms","Colors":"green"},{"Courses":"Advanced Applied Statistics and Multivariate Calculus","Colors":"red"},
+    {"Courses":"Advanced Data Systems","Colors":"blue"},{"Courses":"Advanced Machine Learning","Colors":"green"},{"Courses":"Advanced Natural Language Processing and Deep Learning","Colors":"blue"},
+    {"Courses":"Advanced Network Science","Colors":"red"},{"Courses":"Advanced Programming","Colors":"green"},{"Courses":"Advanced Robotics","Colors":"red"},{"Courses":"Advanced Security","Colors":"blue"},
+    {"Courses":"Advanced Service Design","Colors":"green"},{"Courses":"Advanced Software Analysis","Colors":"blue"},{"Courses":"Advanced Software Engineering","Colors":"red"},{"Courses":"Advanced Topics in Game Studies","Colors":"green"},
+    {"Courses":"Algorithm Design","Colors":"red"},{"Courses":"Algorithms for Game Development","Colors":"blue"},{"Courses":"Applied Algorithms","Colors":"green"},{"Courses":"Applied information Security","Colors":"blue"},
+    {"Courses":"Avancerede designprocesser","Colors":"red"},{"Courses":"Big Data Management (Technical)","Colors":"green"},{"Courses":"Blockchain Economics","Colors":"red"},{"Courses":"Computational Literacies","Colors":"blue"},
+    {"Courses":"Critical Big Data Management: Second Part of Specialisation","Colors":"green"},{"Courses":"DADIU Curriculum","Colors":"blue"},{"Courses":"DADIU Project","Colors":"red"},{"Courses":"Data Mining","Colors":"green"},
+    {"Courses":"Data in Design","Colors":"red"},{"Courses":"Data in the Wild: Wrangling and Visualising Data","Colors":"blue"},{"Courses":"Data Automation and Social Justice","Colors":"green"},{"Courses":"Deep Learning for Games and Simulations","Colors":"blue"},
+    {"Courses":"Designing Aesthetic User Experiences","Colors":"red"},{"Courses":"Discrete Mathematics","Colors":"green"},{"Courses":"Distributed Systems","Colors":"red"},{"Courses":"Game Programming","Colors":"blue"},{"Courses":"Games & Culture","Colors":"green"},
+    {"Courses":"How to make (almost) anything","Colors":"blue"},{"Courses":"IT and Green Transitions","Colors":"red"},{"Courses":"Introduction to Database Systems","Colors":"green"},{"Courses":"Introduction to Machine Learning","Colors":"red"},
+    {"Courses":"Introduction to Programming","Colors":"blue"},{"Courses":"Introductory Programming","Colors":"green"},{"Courses":"Making Games","Colors":"blue"},{"Courses":"Navigating Complexity","Colors":"red"},{"Courses":"Operating Systems and C","Colors":"green"},
+    {"Courses":"Organisational Change","Colors":"red"},{"Courses":"Perspectives on Games","Colors":"blue"},{"Courses":"Play Lab","Colors":"green"},{"Courses":"Practical Concurrent and Parallel Programming","Colors":"blue"},{"Courses":"Programming Mobile Applications","Colors":"red"},
+    {"Courses":"Programming for Designers","Colors":"green"},{"Courses":"Reassembling Innovation","Colors":"red"},{"Courses":"Security 1","Colors":"blue"},{"Courses":"Seminars in Data Science","Colors":"green"},
+    {"Courses":"Service Design - Management and Implementation","Colors":"blue"},{"Courses":"Situating Interactions","Colors":"red"},{"Courses":"Software Ecosystems","Colors":"green"},{"Courses":"Software Engineering","Colors":"red"},
+    {"Courses":"Technical Interaction Design","Colors":"blue"},{"Courses":"The Digital State in Practice","Colors":"green"},{"Courses":"UX Design II","Colors":"blue"},{"Courses":"Users in Context","Colors":"red"},{"Courses":"Værdier og etik i design","Colors":"green"}
+  ]
+
+  let courseArray = courseObj.map(({ Courses }) => Courses)
+
+  function courseToHTML(item, index){
+    return <div key={index}>
+      <input type="checkbox" id={item} name={item} value={item} />
+      <label for={item}> {item}</label><br></br>
+    </div>
+
+  }
+
+  const courseList = courseArray.map(courseToHTML);
+
+
+    // COURSE TO DATABASE
+//   const counter2 = useRef(0)
+//   if (counter2.current === 1){
+
+//     for (let x = 0; x < 1; x++) { 
+        
+//         let courseArray = courseObj.map(({ Courses }) => Courses)
+//         let colorAray = courseObj.map(({ Colors }) => Colors)
+        
+//         for (let i = 0; i < courseArray.length; i++) { 
+//             let NicknameA = new Parse.Object('Course');
+//             NicknameA.set('CourseTitle', courseArray[i]);
+//             NicknameA.set('CourseColor', colorAray[i]);
+//             NicknameA = NicknameA.save();
+//         }
+        
+//     }
+// }
+
+// counter2.current = counter2.current + 1;
 
   // Handling the first name change
   const handleFirstName = (e) => {
@@ -97,6 +150,8 @@ const renderErrorMessage = () =>(
           <h3>Live chat platform for ITU students</h3>
       </div>
 
+      <label htmlFor="cars">Choose a car:</label>
+
       <div className="input-wrapper">
         <h1 className="input-header">Create account</h1>
         
@@ -127,6 +182,13 @@ const renderErrorMessage = () =>(
             <input placeholder="Type here..." onChange={handleRepeatPassword} type="password" value={repeatPassword}></input>
           </div>
 
+        </div>
+        
+        <div className="input-container ">
+          <h3>Choose your courses</h3>
+          <form className="course-list">
+            {courseList}
+          </form>
         </div>
 
         <div className="input-btn">
