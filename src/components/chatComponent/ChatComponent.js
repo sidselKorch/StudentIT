fc35367ee929ac2c75ee8bc7e3bea4bb606bc05f
wimrijkss <80752989+wimrijkss@users.noreply.github.com>
2022-12-06 15:57:24 +0100

    Close function in chat

--- src/components/chatComponent/ChatComponent.js
+++ src/components/chatComponent/ChatComponent.js
@@ -5,13 +5,13 @@ import { ReceiverIdContext } from '../../contexts/ReceiverIdContext';
 import Parse from 'parse/dist/parse.min.js';
 
 function ChatComponent(props) {
-
+  
   const [ReceiverId, setReceiverId] = useContext(ReceiverIdContext)
-
+  
   const [ userData, setUserdata ] = useState([])
-
+  
   const [ receiverName, setReceiverName ] = useState("")
-
+  
   const [ receiverInitials, setReceiverInitials ] = useState("")
 
   async function getReceiverName(){
@@ -21,20 +21,32 @@ function ChatComponent(props) {
     console.log(UserQueryResult.get('firstName'))
     setReceiverName(UserQueryResult.get('firstName') + " " + UserQueryResult.get('lastName'))
     setReceiverInitials(UserQueryResult.get('firstName').substring(0,1) + UserQueryResult.get('lastName').substring(0,1))
-
   }
 
   useEffect(() => {
     getReceiverName()
   });
 
+  // function resetReceiverId(){
+  //   setReceiverId("")
+  // }
+
   return (
     <div className="chat-component">
       <div className="chat-header-container">
-        <div className = "user-icon">
-          <h3 className="user-icon-text" >{receiverInitials} </h3>
+        <div className="chat-header-info">
+          <div className = "user-icon">
+            <h3 className="user-icon-text" >{receiverInitials} </h3>
+          </div>
+          <h2>{receiverName}</h2>
         </div>
-        <h2>{receiverName}</h2>
+        {/* <button className="btn" onClick={() => resetReceiverId()}>Close Chat</button> */}
+
+        <div className="dot-settings">
+          <div className="dot"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
       </div>
       <ChatSetup />
     </div>
